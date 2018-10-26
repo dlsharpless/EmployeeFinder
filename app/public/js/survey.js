96f68b5c43d7f11ad82b339279319a3b98431c9f
dlsharpless <10237785+dlsharpless@users.noreply.github.com>
2018-10-26 13:46:03 -0500

    building algorithm

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -1,14 +1,14 @@
 const submit = function () {
-    let input = []
+    let scores = []
     for (i = 1; i <= 10; i++) {
-        input.push(parseInt($(`#q${i}`)[0].value));
+        scores.push($(`#q${i}`)[0].value);
     }
     $.ajax({
         url: "/api/employees",
         method: "POST",
-        data: input
+        data: { scores: scores }
     }).then(function (response) {
-
+        console.log(response)
     })
 }
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,8 +7,27 @@ router.get("/api/employees", function (req, res) {
 });
 
 router.post("/api/employees", function (req, res) {
-    res.json(req.body);
+    // res.json(req.body);
     // employeeList.push(req.body)
+    console.log("Hi 1");
+    for (i = 0; i < req.body.scores.length; i++) {
+        req.body.scores[i] = parseInt(req.body.scores[i])
+    }
+    let totalDifferences = [];
+    let similarEmployee = [];
+    for (i = 0; i < employeeList.length; i++) {
+        totalDifferences[i] = 0;
+        for (j = 0; j < req.body.scores.length; j++) {
+            totalDifferences[i] += Math.abs(req.body.scores[j] - employeeList[i].scores[j])
+        }
+        if (i=0||totalDifferences[i]<similarEmployee[1]){
+            similarEmployee[0] = i;
+            similarEmployee[1] = totalDifferences[i]
+        }
+    }
+    console.log("Hi 2");
+    console.log(employeeList[similarEmployee[0]]);
+    res.json("Hello")
 });
 
 module.exports = router;
