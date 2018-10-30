bad1c6666f45a6c9c32ba9328edb28db5d36760e
dlsharpless <10237785+dlsharpless@users.noreply.github.com>
2018-10-30 11:37:15 -0500

    required name and updated styling

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -1,5 +1,13 @@
+const invalidInput = function() {
+    $(".modal-title").text(`Incomplete`);
+    $(".modal-body").html(`<p>*Name is a required field.</p>`);
+}
+
 const submit = function () {
     let name = $("#name").val();
+    if (!name) {
+        return invalidInput();
+    }
     let photo = $("#photo").val();
     let scores = []
     for (i = 1; i <= 10; i++) {
@@ -16,6 +24,7 @@ const submit = function () {
         }
     }).then(function (response) {
         console.log("lol");
+        $(".modal-title").text(`Best Match`);
         $(".modal-body").html(`<img src="${response.photo}"><br><p>${response.name}</p>`);
     })
 }
