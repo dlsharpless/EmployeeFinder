7656f11050938352ea9269171f82ba0f2a19934c
dlsharpless <10237785+dlsharpless@users.noreply.github.com>
2018-10-30 19:28:22 -0500

    fixed routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
-router.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
 })
 
-router.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
+router.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
 })
 
 module.exports = router;
